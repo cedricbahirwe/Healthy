2d92a920eea89ebf06decd02b2c59315108d4365
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-07-13 12:35:37 +0200

    Add Profile view

---
+++ Healthy/Healthy/Views/ProfileView.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileView.swift
+//  Healthy
+//
+//  Created by Cédric Bahirwe on 13/07/2021.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Form {
+            
+            FormRowView("Name", text: .constant(""))
+            
+            FormRowView("Age", text: .constant(""))
+            
+            FormRowView("Blood Type", text: .constant(""))
+            
+            Button(action: {
+                
+            }) {
+                Text("Save")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 40)
+                    .foregroundColor(Color(.systemBackground))
+                    .background(Color.primary)
+                    .cornerRadius(10)
+            }
+            .padding(.top)
+        }
+        .navigationTitle("Profile Settings")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ProfileView()
+//                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct FormRowView: View {
+    let title: String
+    @Binding var text: String
+    
+    init(_ title: String, text: Binding<String>) {
+        self.title = title
+        _text = text
+    }
+    
+    var body: some View {
+        HStack {
+            Text("\(title):")
+            
+            TextField(title, text: $text)
+        }
+    }
+}
